ba38fbcf092a1ee9e6b6d561c826ed47ab8ce5d5
ariel-pi <135214280+ariel-pi@users.noreply.github.com>
2024-04-21 09:44:59 +0300

    update settings.py

--- MarketPlace/MarketPlaceWebsite/MarketPlaceWebsite/settings.py
+++ MarketPlace/MarketPlaceWebsite/MarketPlaceWebsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-io99cb9t-pkx1&f=g61nr!h1xsss#h1e_5-jjxgyt2+63@gcj('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
